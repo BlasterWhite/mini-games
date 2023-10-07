72eb979dbb34257e49627b3260384093984aa7c9
BlasterWhite <icarambala@gmail.com>
2023-10-07 20:59:51 +0200

    Added Patch MANY things + First playable game

--- src/components/MinesWeeper/MinesField.vue
+++ src/components/MinesWeeper/MinesField.vue
@@ -24,18 +24,31 @@ const props = defineProps({
   },
 });
 
-const emits = defineEmits(["update:restartTheGame"]);
+const emits = defineEmits(["update:restartTheGame", "win", "lose", "start"]);
 
 const tileCss = computed(() => {
   return `${props.tileSizes}px`;
 });
 
-const playGround = computed(() => {
-  return Array(props.height)
+/*
+ * first array is the row
+ * second array is the column
+ * third array is the cell
+ *    - first is the dev value
+ *    - second is the display value
+ */
+const playGround = ref(
+  Array(props.height)
     .fill()
-    .map(() => Array(props.width).fill(0));
-});
-
+    .map(() => Array(props.width)
+        .fill().map(() => {
+          return {
+            status: 0,
+            value: 0,
+            label: "",
+          };
+        })),
+);
 const numberOfMines = ref(0);
 
 /**
@@ -44,20 +57,24 @@ const numberOfMines = ref(0);
 function generateMines() {
   numberOfMines.value = 0;
   const mines = Math.round(props.width * props.height * props.minesPercent);
-  for (let i = 0; i < mines; i++) {
-    const x = Math.floor(Math.random() * props.width);
-    const y = Math.floor(Math.random() * props.height);
-    if (playGround.value[y][x] === -1) {
+
+  for (let i = 0; numberOfMines.value < mines; i++) {
+    let x = Math.floor(Math.random() * props.width);
+    let y = Math.floor(Math.random() * props.height);
+
+    if (playGround.value[y][x].value === -1) {
       i--;
       continue;
     }
+
+    playGround.value[y][x].value = -1;
+    playGround.value[y][x].label = '💣';
     numberOfMines.value++;
-    playGround.value[y][x] = -1;
   }
 }
 
 /**
- *
+ * Calculate the number of mines around a cell
  * @param {number} x
  * @param {number} y
  * @returns {number}
@@ -76,7 +93,7 @@ function calculMineAround(x, y) {
         // out of bound
         continue;
       }
-      if (playGround.value[y + i][x + j] === -1) {
+      if (playGround.value[y + i][x + j].value === -1) {
         mines++;
       }
     }
@@ -90,15 +107,16 @@ function calculMineAround(x, y) {
 function calculAllMinesAround() {
   for (let y = 0; y < props.height; y++) {
     for (let x = 0; x < props.width; x++) {
-      if (playGround.value[y][x] === -1) {
+      if (playGround.value[y][x].value === -1) {
         // if it's a mine, skip
         continue;
       }
-      playGround.value[y][x] = calculMineAround(x, y);
+      playGround.value[y][x].label = calculMineAround(x, y).toString();
     }
   }
 }
 
+const hasStarted = ref(false);
 /**
  * Reveal a cell
  * @param {number} x
@@ -106,17 +124,23 @@ function calculAllMinesAround() {
  * @param {Event} event
  */
 function reveal(x, y, event) {
-  if (playGround.value[y][x] === -2 || event.target.innerText === "💣") {
+  if (!hasStarted.value) {
+    hasStarted.value = true;
+    emits("start")
+  }
+  if (playGround.value[y][x].status === -2 || playGround.value[y][x].status === -3) {
     return;
   }
   let colored = (y + x) % 2 ? "revealedDark" : "revealed";
   event.target.classList.add(colored);
-  event.target.innerText = playGround.value[y][x];
-  if (playGround.value[y][x] === -1) {
-    alert("You lose");
+  event.target.innerText = playGround.value[y][x].label;
+  if (playGround.value[y][x].value === -1) {
+    // if it's a mine
+    event.target.style.backgroundColor = "#ff0000";
+    emits("lose");
     return;
   }
-  playGround.value[y][x] = -2;
+  playGround.value[y][x].status = -2;
   calculVictory();
 }
 
@@ -126,16 +150,23 @@ function reveal(x, y, event) {
  * @param {number} y
  * @param {Event} event
  */
-function declareMine(x, y, event) {
+function putFlag(x, y, event) {
   event.preventDefault();
-  if (event.target.innerText === "💣") {
+  if(playGround.value[y][x].status === -2 || numberOfMines.value <= 0) return;
+
+  if (playGround.value[y][x].status === -3) { // if it's a flag
+    playGround.value[y][x].status = 0; // reset the dev value
+    playGround.value[y][x].label = calculMineAround(x, y).toString(); // reset the display value
     event.target.innerText = "";
     event.target.style.backgroundColor = "";
     numberOfMines.value++;
     return;
   }
-  event.target.innerText = "💣";
-  event.target.style.backgroundColor = "#ff0000";
+
+  playGround.value[y][x].status = -3;
+  playGround.value[y][x].label = "🚩";
+  event.target.innerText = playGround.value[y][x].label;
+  event.target.style.backgroundColor = "#d2bc41";
   numberOfMines.value--;
   calculVictory();
 }
@@ -144,60 +175,41 @@ function calculVictory() {
   let victory = true;
   for (let y = 0; y < props.height; y++) {
     for (let x = 0; x < props.width; x++) {
-      if (playGround.value[y][x] === -1) {
+      if (playGround.value[y][x].value === -1) {
         // if it's a mine, skip
         continue;
       }
-      if (playGround.value[y][x] !== -2) {
+      if (playGround.value[y][x].status !== -2) {
         victory = false;
       }
     }
   }
   if (victory) {
-    alert("You win");
+    emits("win");
   }
 }
 
 watch(
-  () => props.height,
-  () => {
-    generateMines();
-    calculAllMinesAround();
-  },
-);
-
-watch(
-  () => props.width,
-  () => {
-    generateMines();
-    calculAllMinesAround();
-  },
-);
-
-watch(
-  () => props.minesPercent,
-  () => {
-    generateMines();
-    calculAllMinesAround();
-  },
-);
-
-watch(
   () => props.restartTheGame,
   () => {
+    if (!props.restartTheGame) return;
     restart();
     emits("update:restartTheGame", false);
   },
 );
 
 function restart() {
-  console.log("restart !!")
-  document.querySelectorAll(".cell").forEach((cell) => {
-    cell.classList.remove("revealed");
-    cell.classList.remove("revealedDark");
-    cell.innerText = "";
-    cell.style.backgroundColor = "";
-  });
+  hasStarted.value = false;
+  playGround.value = Array(props.height)
+    .fill()
+    .map(() => Array(props.width)
+        .fill().map(() => {
+          return {
+            status: 0,
+            value: 0,
+            label: "",
+          };
+        }));
   generateMines();
   calculAllMinesAround();
 }
@@ -208,14 +220,14 @@ calculAllMinesAround();
 
 <template>
   Mines: {{ numberOfMines }} 💣<br />
-  <div class="minesweeper">
+  <div class="minesweeper" v-if="!restartTheGame">
     <div v-for="(row, indexRow) in playGround" class="row">
       <div
         class="cell"
         :class="(indexRow + indexCell) % 2 ? 'darkgreen' : 'green'"
         v-for="(cell, indexCell) in row"
         @click="reveal(indexCell, indexRow, $event)"
-        @contextmenu="declareMine(indexCell, indexRow, $event)"
+        @contextmenu="putFlag(indexCell, indexRow, $event)"
       ></div>
     </div>
   </div>
@@ -250,7 +262,7 @@ div.cell.revealedDark {
   background-color: #e8d9ae;
 }
 
-div.cell:not(.revealed):hover {
+div.cell:not(.revealed):not(.revealedDark):hover {
   background-color: #53cc28;
 }
 </style>
@@ -260,6 +272,9 @@ div.cell:not(.revealed):hover {
   flex-direction: column;
   align-items: center;
   justify-content: center;
+  border-radius: 12px;
+  overflow: hidden;
+  box-shadow: 4px 4px 8px 0px rgba(0, 0, 0, 0.5);
 }
 
 .row {
--- src/views/MinesWeeperView.vue
+++ src/views/MinesWeeperView.vue
@@ -1,19 +1,14 @@
 <script setup>
 import MinesWeeper from "@/components/MinesWeeper/MinesField.vue";
 import MinesWeeperSettings from "@/components/MinesWeeper/MinesWeeperSettings.vue";
-import { ref, watch } from "vue";
+import { computed, ref, watch } from "vue";
 
-// TODO: add a function to reset the game
 // TODO: color the number of mines around the cell
 // TODO: add a timer
 // TODO: add local storage to save the best time
 // TODO: add Animation
-// TODO: add settings
 // TODO: add Confetti when win
-
-// FIXME: unSet a flag when a cell is revealed remove the number of mine around the cell
-// FIXME: Number of flag can not be more than the number of mines
-// FIXME: Spam click restart the game cause lag
+// TODO: add logic where you can't lose on the first click
 
 const width = ref(10);
 const height = ref(10);
@@ -27,6 +22,9 @@ const modalSettingsTileSizes = ref(tileSizes.value);
 
 const isModalSettingsOpen = ref(false);
 
+/**
+ * Function called when the settings are reset
+ */
 function reset() {
   width.value = 4;
   height.value = 4;
@@ -34,8 +32,10 @@ function reset() {
   tileSizes.value = 30;
 }
 
+/**
+ * Function called when the settings are changed
+ */
 function change() {
-  console.log("change");
   width.value = modalSettingsWidth.value;
   height.value = modalSettingsHeight.value;
   minesPercent.value = modalSettingsMinesPercent.value;
@@ -44,27 +44,64 @@ function change() {
 
 const restartTheGame = ref(false);
 function restart() {
-  console.log("restart");
+  stopTimer();
+  timer.value = 0;
+  showModal.value = false;
   restartTheGame.value = true;
 }
 
-watch(modalSettingsWidth, (value) => {
-  console.log(value);
+const showModal = ref(false);
+const modalContent = ref({});
+
+/**
+ * Function called when the player lose
+ */
+function lose() {
+  stopTimer();
+  modalContent.value = {
+    title: "You lose",
+    message: "You lose, try again !",
+  };
+  showModal.value = true;
+}
+
+/**
+ * Function called when the player win
+ */
+function win() {
+  stopTimer();
+  modalContent.value = {
+    title: "You win",
+    message: "You win, congratulations !",
+  };
+  showModal.value = true;
+}
+
+const timer = ref(0);
+let interval;
+function startTimer() {
+  interval = setInterval(() => {
+    timer.value += 1;
+  }, 1000);
+}
+
+function stopTimer() {
+  clearInterval(interval);
+}
+
+const timerFormatted = computed(() => {
+  const minutes = Math.floor(timer.value / 60);
+  const seconds = timer.value % 60;
+  const hours = Math.floor(timer.value / 3600);
+  return `${hours}:${minutes < 10 ? `0${minutes}` : minutes}:${
+    seconds < 10 ? `0${seconds}` : seconds
+  }`;
 });
 </script>
 
 <template>
-  {{ width }}
-  {{ height }}
-  {{ minesPercent }}
-  {{ tileSizes }}<br>
-  {{ modalSettingsWidth }}
-  {{ modalSettingsHeight }}
-  {{ modalSettingsMinesPercent }}
-  {{ modalSettingsTileSizes }}
   <h1>MinesWeeper</h1>
-  <button @click="isModalSettingsOpen = true">Settings</button>
-  <button @click="restart">Restart</button>
+  <p class="timer">{{ timerFormatted }}</p>
   <MinesWeeperSettings
     v-if="isModalSettingsOpen"
     @close="isModalSettingsOpen = false"
@@ -81,7 +118,93 @@ watch(modalSettingsWidth, (value) => {
     :mines-percent="minesPercent"
     :tile-sizes="tileSizes"
     v-model:restart-the-game="restartTheGame"
+    @win="win"
+    @lose="lose"
+    @start="startTimer"
   />
+  <div v-if="showModal" class="modal">
+    <div class="modal-content">
+      <span @click="showModal = false" class="close">&times;</span>
+      <h2>{{ modalContent.title }}</h2>
+      <p>{{ modalContent.message }}</p>
+      <button @click="restart">Restart</button>
+    </div>
+  </div>
+  <div class="actions">
+    <button @click="isModalSettingsOpen = true">Settings</button>
+    <button @click="restart">Restart</button>
+  </div>
 </template>
 
-<style scoped></style>
+<style scoped>
+.modal {
+  display: block;
+  position: fixed;
+  z-index: 1;
+  padding-top: 100px;
+  left: 0;
+  top: 0;
+  width: 100%;
+  height: 100%;
+  overflow: auto;
+  background-color: rgb(0, 0, 0, 0.4);
+
+  & .modal-content {
+    background-color: #fefefe;
+    color: #1a1a1a;
+    margin: auto;
+    border-radius: 12px;
+    width: 80%;
+    max-width: 500px;
+    display: flex;
+    flex-direction: column;
+    align-items: center;
+    justify-content: center;
+    padding: 20px;
+    gap: 10px;
+
+    & h2 {
+      margin: 0;
+    }
+
+    & p {
+      margin: 0;
+    }
+
+    & .close {
+      margin: 0 4px;
+      color: #aaaaaa;
+      float: right;
+      align-self: flex-end;
+      font-size: 28px;
+      font-weight: bold;
+      cursor: pointer;
+    }
+
+    & .close:hover,
+    & .close:focus {
+      color: #000;
+      text-decoration: none;
+      cursor: pointer;
+    }
+  }
+}
+
+.actions {
+  display: flex;
+  flex-direction: row;
+  align-items: center;
+  justify-content: center;
+  gap: 10px;
+
+  & > button {
+    margin: 8px;
+  }
+}
+
+.timer {
+  font-size: 24px;
+  font-weight: bold;
+  text-align: center;
+}
+</style>
